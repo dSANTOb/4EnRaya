ce9947a2fcd7746f5be60ad29fc16ea734c65976
dSANTOb <santiago.vegarobles@ucr.ac.cr>
2024-06-04 03:31:09 -0600

    Arreglo de conflictos

--- include/Tablero.hh
+++ include/Tablero.hh
@@ -8,17 +8,18 @@ private:
     //TODO: El tablero no debe ser constante, sino que cambia con la ventana
     //Vector int que almacena el estado del tablero (Una matriz de enteros) en grid
     std::vector<std::vector<int>> grid;
-    const int filas = 6;
-    const int columnas = 7;
+    int filas;
+    int columnas;
 
 public:
-    Tablero();
+    Tablero(int filas = 6, int columnas = 7);
     bool colocarFicha(int columna, int jugador);
     void imprimirTablero() const;
     int verificarGanador() const;
     
     // Método para obtener el estado actual del tablero
     const std::vector<std::vector<int>>& getGrid() const;
+    void redimensionar(int nuevasFilas, int nuevasColumnas);
 };
 
 #endif // TABLERO_HH
