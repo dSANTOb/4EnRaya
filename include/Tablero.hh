e50d48c65e8374c0b8908b75c80a8dbb09e1bcc2
dSANTOb <santiago.vegarobles@ucr.ac.cr>
2024-06-04 02:30:09 -0600

    Agregando srcs al CMakeLists

--- include/Tablero.hh
+++ include/Tablero.hh
@@ -8,14 +8,14 @@ private:
     //TODO: El tablero no debe ser constante, sino que cambia con la ventana
     //Vector int que almacena el estado del tablero (Una matriz de enteros) en grid
     std::vector<std::vector<int>> grid;
-    const int filas = 6;
-    const int columnas = 7;
 
 public:
     Tablero();
     bool colocarFicha(int columna, int jugador);
     void imprimirTablero() const;
     int verificarGanador() const;
+    const int filas = 6;
+    const int columnas = 7;
     
     // Método para obtener el estado actual del tablero
     const std::vector<std::vector<int>>& getGrid() const;
