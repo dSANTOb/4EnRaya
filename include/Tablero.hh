b76ba5239ca96d6be609ca188107838b89474a98
dSANTOb <santiago.vegarobles@ucr.ac.cr>
2024-06-11 23:42:14 -0600

    Optimizacion de verificar ganador

--- include/Tablero.hh
+++ include/Tablero.hh
@@ -8,17 +8,17 @@ private:
     //TODO: El tablero no debe ser constante, sino que cambia con la ventana
     //Vector int que almacena el estado del tablero (Una matriz de enteros) en grid
     std::vector<std::vector<int>> grid;
-    int filas;
-    int columnas;
+    bool esValido(int fila, int columna) const;
+    bool verificarLinea(int fila, int columna, int deltaFila, int deltaColumna, int jugador) const; // Método auxiliar
 
 public:
-    Tablero(int filas = 6, int columnas = 7);
+    Tablero(int filas, int columnas);
     bool colocarFicha(int columna, int jugador);
     void imprimirTablero() const;
-    int verificarGanador() const;
+    int verificarGanador(int fila, int columna) const;
 
     const std::vector<std::vector<int>>& getGrid() const;
     void redimensionar(int nuevasFilas, int nuevasColumnas);
 };
 
-#endif // TABLERO_HH
+#endif
